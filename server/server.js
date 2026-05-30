aef8a73e68a631beec67198e8c4d165ac6dcb955
Yash Pratap Kulshrestha <147393761+yash3605@users.noreply.github.com>
2026-05-30 19:29:35 +0530

    Fix Express 5 wildcard route syntax for production

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get('/api/health', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
+  app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 } else {
